551b305180830f10130baebf2e7dc6f8b9885bee
acurtis28 <Curtain@Austins-MacBook-Pro.local>
2016-01-28 17:05:24 -0500

    add without string

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var firstNumber = prompt("Enter the first number");
 firstNumber = Number(firstNumber);
-
+if (isNaN(firstNumber)) {
+  alert("this is not a number")
+}
 
 var secondNumber = prompt("Enter the second number");
 secondNumber = Number(secondNumber);
+if (isNaN(secondNumber)) {
+  alert("this is not a number")
+}
 
-
-var sum = firstNumber +secondNumber;
+var sum = firstNumber + secondNumber;
 alert("Sum of the two number is \n "+sum);
